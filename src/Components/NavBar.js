a1c07033485dbb9b9e901be566790ee6321b5b90
manoti <manotimike@gmail.com>
2023-05-07 19:15:24 +0300

    nav buttons styled

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -3,8 +3,8 @@ import { NavLink } from 'react-router-dom'
 
 const linkStyles = {
   display: 'inline-block',
-  width: '150px',
-  padding: '12px',
+  width: '120px',
+  padding: '10px',
   margin: '0 6px 6px',
   border: '1px solid black',
   borderRadius: '10px',
